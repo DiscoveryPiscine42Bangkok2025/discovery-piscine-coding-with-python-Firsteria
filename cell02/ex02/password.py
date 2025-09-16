fd982f977872dfe24be1628d1967db6a4bdc3d36
Firsteria <141379353+Firsteria@users.noreply.github.com>
2025-09-16 13:29:12 +0700

    ex3-2
    yesyesyes

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+password = "Python is awesome"
+
+ent_password = input()
+
+if ent_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = num1 * num2
+
+if result > 0:
+    print("The result is positive")
+elif result < 0:
+    print("The result is negative")
+else:
+    print("The result is positive and negative")
