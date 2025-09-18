65d902c73ad8f455bd5ea425628f7813b6a01537
Firsteria <141379353+Firsteria@users.noreply.github.com>
2025-09-18 17:53:48 +0700

    rush changename

--- Rush/main.py
+++
@@ -1,8 +0,0 @@
-def find_king(board):
-    #Find the king Positions
-    lines = board.strip().split('\n')
-    for row in range(len(lines)):
-        for col in range(len(lines[row])):
-            if lines[row][col] == 'K':
-                return row, col
-    return None
